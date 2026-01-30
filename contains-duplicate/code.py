0e8bb28eb831cf71ec382ba6cc8e862b9bfa5da9
Catherine <catherineliu2479@gmail.com>
2026-01-30 18:18:54 +1100

    contains-duplicate easy

---
+++ contains-duplicate/code.py
@@ -0,0 +1,21 @@
+import json
+from typing import List
+
+class ContainsDuplicate:
+  def __init__(self, nums):
+    self.nums = nums
+  def hasDuplicate(self, nums: List[int]) -> bool:
+    return len(nums) > len(set(nums))
+
+def main():
+  str = input()
+  if len(str) < 2 or str[0] != '[' or str[-1] != ']':
+    print("Please enter an array in the format of [a,b,c...x,y,z] where a..z are numbers\n")
+    exit(1)
+
+  nums = json.loads(str)
+  obj = ContainsDuplicate(nums)
+  print(obj.hasDuplicate(nums));
+
+if __name__ == "__main__":
+  main()
